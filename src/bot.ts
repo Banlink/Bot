ae41f3202b3356bafe0b7bb0d93367cb1edf8fba
Whanos <github@sergal.fun>
2021-09-20 13:22:52 +0100

    first push, start of ts

---
+++ src/bot.ts
@@ -0,0 +1,23 @@
+import {Client} from "@typeit/discord"
+import {config} from "dotenv"
+import {resolve} from "path"
+
+config({path: resolve(__dirname, "../.env")})
+
+
+export class Main {
+    private static _client: Client;
+
+    static get Client(): Client {
+        return this._client;
+    }
+
+    static start() {
+        this._client = new Client();
+        this._client.login(process.env.BOT_TOKEN,
+            `${__dirname}/discords/*.ts`,
+            `           ${__dirname}/discords/*.js`)
+    }
+}
+
+Main.start();
